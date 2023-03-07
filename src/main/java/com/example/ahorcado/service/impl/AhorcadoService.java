e4a5a0b59065fca6a4d17a5708d019004053e477
Ramon Vazquez <ramon.vazquez@babelgroup.com>
2023-03-07 17:32:21 +0100

    implementados los metodos del service

--- src/main/java/com/example/ahorcado/service/impl/AhorcadoService.java
+++ src/main/java/com/example/ahorcado/service/impl/AhorcadoService.java
@@ -2,14 +2,24 @@ package com.example.ahorcado.service.impl;
 
 import com.example.ahorcado.service.IAhorcadoService;
 
+import java.util.Random;
+
 public class AhorcadoService implements IAhorcadoService {
     @Override
     public String getPalabraSecreta() {
-        return null;
+        String [] palabras = {"melon","platano","naranja","manzana","sandia","pera","kiwi","mango","pimiento","cebolla"};
+        Random r = new Random();
+        int n = r.nextInt(palabras.length);
+        return palabras[n];
     }
 
     @Override
     public char[] getGuionesFromPalabra(String palabra) {
-        return new char[0];
+        int letrasPalabraSecreta = palabra.length();
+        char[] palabraGuiones = new char[letrasPalabraSecreta];
+        for(int i = 0; i < palabraGuiones.length; i++){
+            palabraGuiones[i] = '_';
+        }
+        return palabraGuiones;
     }
 }
