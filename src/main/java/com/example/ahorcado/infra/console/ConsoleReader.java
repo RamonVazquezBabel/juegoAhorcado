a2a521d5fe94f3048131c8d71dea575bd42af5fe
Ramon Vazquez <ramon.vazquez@babelgroup.com>
2023-03-07 18:09:36 +0100

    Beans realizados y consola preparada para realizar

--- src/main/java/com/example/ahorcado/infra/console/ConsoleReader.java
+++ src/main/java/com/example/ahorcado/infra/console/ConsoleReader.java
@@ -1,4 +1,26 @@
 package com.example.ahorcado.infra.console;
 
+import com.example.ahorcado.domain.Ahorcado;
+import com.example.ahorcado.service.impl.AhorcadoService;
+
+import javax.annotation.PostConstruct;
+
 public class ConsoleReader {
+    private AhorcadoService ahorcadoService;
+    public ConsoleReader(AhorcadoService ahorcadoService){
+        this.ahorcadoService = ahorcadoService;
+    }
+
+    @PostConstruct
+    public void init(){
+
+    }
+
+   /* do{
+
+    }while(true){
+
+    }
+
+    */
 }
--- src/main/java/com/example/ahorcado/infra/console/config/AhorcadoConfig.java
+++ src/main/java/com/example/ahorcado/infra/console/config/AhorcadoConfig.java
@@ -1,4 +1,23 @@
 package com.example.ahorcado.infra.console.config;
 
+import com.example.ahorcado.infra.console.ConsoleReader;
+import com.example.ahorcado.repository.impl.AhorcadoRepository;
+import com.example.ahorcado.service.impl.AhorcadoService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AhorcadoConfig {
+    @Bean
+    AhorcadoRepository getAhorcadoRepository(){
+        return new AhorcadoRepository();
+    }
+    @Bean
+    AhorcadoService getAhorcadoService(){
+        return new AhorcadoService(getAhorcadoRepository());
+    }
+    @Bean
+    ConsoleReader getConsoleReader(){
+        return new ConsoleReader(getAhorcadoService());
+    }
 }
