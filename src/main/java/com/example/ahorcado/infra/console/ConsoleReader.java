8a66034274778d33d04ea62b8030dc15edf26c71
Ramon Vazquez <ramon.vazquez@babelgroup.com>
2023-03-07 18:26:02 +0100

    consola empezada, pendiente el caso del juego

--- src/main/java/com/example/ahorcado/infra/console/ConsoleReader.java
+++ src/main/java/com/example/ahorcado/infra/console/ConsoleReader.java
@@ -4,6 +4,7 @@ import com.example.ahorcado.domain.Ahorcado;
 import com.example.ahorcado.service.impl.AhorcadoService;
 
 import javax.annotation.PostConstruct;
+import java.util.Scanner;
 
 public class ConsoleReader {
     private AhorcadoService ahorcadoService;
@@ -13,7 +14,35 @@ public class ConsoleReader {
 
     @PostConstruct
     public void init(){
+        Scanner sc = new Scanner(System.in);
+        int opcion = 1;
+        String entradaConsola;
+        while(opcion!=0){
+            System.out.println("Menu por consola");
+            System.out.println("1- Iniciar ahorcado");
+            System.out.println("2- Salir");
+            entradaConsola = sc.next();
 
+            try{
+                opcion = Integer.parseInt(entradaConsola);
+            }catch(Exception e){
+                System.out.println("Opcion no valida, ingrese un numero");
+            }
+            switch (opcion){
+                case 1:
+                    String palabraSecreta = ahorcadoService.getPalabraSecreta();
+                    do{
+
+                    }while(){
+
+                }
+                    break;
+                case 2:
+                    System.out.println("Hasta luego");
+
+                    break;
+            }
+        }
     }
 
    /* do{
